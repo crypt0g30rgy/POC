8292e73434e261604c7c288bfea1ee8307489161
George Maina {{@crypt0g30rgy}} <72516596+crypt0g30rgy@users.noreply.github.com>
2022-12-30 23:43:45 +0300

    Add files via upload

---
+++ enumerate_iam/bruteforce_tests.py
@@ -0,0 +1,42 @@
+BRUTEFORCE_TESTS = {
+    "firehose": [
+        "list_delivery_streams"
+    ],
+    "sts": [
+        "get_caller_identity",
+        "get_session_token"
+    ],
+    "dynamodb": [
+        "list_tables",
+        "describe_endpoints",
+        "describe_limits",
+        "list_backups",
+        "list_contributor_insights",
+        "list_exports",
+        "list_global_tables",
+        "list_imports",
+        "list_tables"
+    ],
+    "iam": [
+        "get_account_authorization_details",
+        "get_account_password_policy",
+        "get_account_summary",
+        "get_credential_report",
+        "get_user",
+        "list_access_keys",
+        "list_account_aliases",
+        "list_groups",
+        "list_instance_profiles",
+        "list_mfa_devices",
+        "list_open_id_connect_providers",
+        "list_policies",
+        "list_roles",
+        "list_saml_providers",
+        "list_server_certificates",
+        "list_service_specific_credentials",
+        "list_signing_certificates",
+        "list_ssh_public_keys",
+        "list_users",
+        "list_virtual_mfa_devices"
+    ]
+}
